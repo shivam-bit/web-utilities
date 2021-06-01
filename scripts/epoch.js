50bd4b50a8df89b88d59fba83dce109502c0f7b8
shivam singh <shivama5tronot@gmail.com>
2021-06-01 21:17:17 +0530

    added epoch timestamp & language translator

--- scripts/epoch.js
+++ scripts/epoch.js
@@ -1,61 +1,14 @@
 /* eslint-disable no-console */
-const timestampToHuman = (timestamp) => {
-    const HumanDate = new Date(timestamp*1000);
-    console.log(HumanDate);
-    return HumanDate;
-};
-
-function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return time;
-}
-
-// console.log(timeConverter(0));
-
-// console.log(new Date(1));
-// const a = 1;
-// const HumanDate = new Date(a);
-// console.log(HumanDate);
 
 window.onload = () => {
-    const dateTimeInput = document.querySelector('#datetime');
+    const dateTimeInputBox = document.querySelector('#datetime');
     const timestampInput = document.querySelector('#timestamp');
-    dateTimeInput.addEventListener('input', (e) => {
-        console.log(e.target.value);
-        // timestampToHuman(e.target.value);
+    dateTimeInputBox.addEventListener('input', (e) => {
+        const dateTimeInput = Date.parse(e.target.value);
+        timestampInput.value = dateTimeInput / 1000;
     });
     timestampInput.addEventListener('input', (e) => {
-        // console.log(e.target.value);
-        // console.log(typeof 1622062610);
-        const HumanDate = timestampToHuman(e.target.value);
-        dateTimeInput.value=HumanDate;
-        // console.log(HumanDate);
-        // console.log(timestampInput.value);
+        const DateObj = new Date(e.target.value * 1000);
+        dateTimeInputBox.value = DateObj.toISOString().replace('Z', '');
     });
-    // console.log(HumanDate);
-    // rgbRawTxt.addEventListener('input', (e) => {
-    //     const hexVal = RGBTextProcessor(e.target.value);
-    //     if (hexVal !== hexRawTxt.textContent) {
-    //         hexRawTxt.value = hexVal;
-    //         colorBox.style.backgroundColor = hexVal;
-    //         colorBox.style.display = 'flex';
-    //     }
-    // });
-    // hexRawTxt.addEventListener('input', (e) => {
-    //     const rgbVal = hexToRGB(e.target.value);
-    //     if (rgbVal !== rgbRawTxt.textContent) {
-    //         rgbRawTxt.value = rgbVal;
-    //         colorBox.style.backgroundColor = rgbVal;
-    //         colorBox.style.display = 'flex';
-    //     }
-    // });
-};
-// window.addEventListener('load', loadFunc);
+}
